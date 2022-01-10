5d5eef0a3cc99fa1a92389b20357503657aa5401
Ruth Cheesley <ruth.cheesley@acquia.com>
2022-01-10 10:53:13 +0000

    Add missing config settings :blush:

--- docs/conf.py
+++ docs/conf.py
@@ -26,6 +26,11 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'xref',
+    'phpdomain',
+    'sphinx_rtd_theme',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.autosectionlabel',
 ]
 
 intersphinx_mapping = {
@@ -38,7 +43,11 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
