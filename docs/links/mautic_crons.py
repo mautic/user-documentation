7f2d2cabb5796295865f9a56f956f84d530a3c3e
Ruth Cheesley <ruth.cheesley@acquia.com>
2022-03-25 18:31:53 +0000

    overview and setup resources

---
+++ docs/links/mautic_crons.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "Cron jobs" 
+link_text = "cron jobs" 
+link_url = "https://docs.mautic.org/setup/cron-jobs" 
+
+link.xref_links.update({link_name: (link_text, link_url)})
