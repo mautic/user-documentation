8def5df99922a584bf08041bedb5b248e6055d0a
Ruth Cheesley <ruth.cheesley@acquia.com>
2022-04-03 12:44:33 +0100

    Add mautic 3 upgrade docs

---
+++ docs/links/mautic_3_upgrade_file.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "Mautic 3 Upgrade" 
+link_text = "Mautic 3 Upgrade 
+link_url = "https://github.com/mautic/mautic/blob/3.x/UPGRADE-3.0.md#configuration" 
+
+link.xref_links.update({link_name: (link_text, link_url)})
