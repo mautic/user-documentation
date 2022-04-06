00431d34a08f7a03f4644f8e755fd6777c59d4d1
Ruth Cheesley <ruth.cheesley@acquia.com>
2022-04-06 17:46:35 +0200

    Push notifications

---
+++ docs/links/onesignal_android.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "OneSignal"
+link_text = "OneSignal"
+link_url = "https://documentation.onesignal.com/docs/android-sdk-setup"
+
+link.xref_links.update({link_name: (link_text, link_url)})
