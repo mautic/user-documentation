64734e724774fc183bc9f82129d57eb6af77a2a8
Favour Kelvin <favourkelvin17@gmail.com>
2022-04-12 01:26:54 +0100

    Delete github_issues.py

--- docs/links/github_issues.py
+++
@@ -1,8 +0,0 @@
-
-from . import link
-
-link_name = "Github issues" 
-link_text = "Github issues" 
-link_url = "https://github.com/mautic/user-documentation" 
-
-link.xref_links.update({link_name: (link_text, link_url)})
