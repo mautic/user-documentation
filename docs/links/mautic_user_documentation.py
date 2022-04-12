0046a8124fbb7df922aa92f9d102d9d62dfac396
Favour Kelvin <favourkelvin17@gmail.com>
2022-04-12 01:31:08 +0100

    Create mautic_user_documentation.py

---
+++ docs/links/mautic_user_documentation.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "Mautic GitHub User Documentation" 
+link_text = "Mautic User Documentation" 
+link_url = "https://github.com/mautic/user-documentation" 
+
+link.xref_links.update({link_name: (link_text, link_url)})
