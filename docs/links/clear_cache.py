3f79be6d462c337d0957b7c0a3c8a5c553da5abd
Favour Chibueze <chibueze.favour.o@gmail.com>
2022-09-29 03:12:20 +0000

    added contact tracking

---
+++ docs/links/clear_cache.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "clearing the symfony cache" 
+link_text = "clearing the symfony cache" 
+link_url = "https://developer.mautic.org/#cache" 
+
+link.xref_links.update({link_name: (link_text, link_url)})
---
+++ docs/links/private_network.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "private network" 
+link_text = "private network" 
+link_url = "https://en.wikipedia.org/wiki/Private_network" 
+
+link.xref_links.update({link_name: (link_text, link_url)})
