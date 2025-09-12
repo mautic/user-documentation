3afcd0294e24e643d46d007c8a41693b16ae0fda
Ayu Adiati <retno.ayu98@gmail.com>
2025-09-12 09:14:01 +0200

    change link to fix 404 error for maxmind ccpa

--- docs/links/maxmind_ccpa.py
+++ docs/links/maxmind_ccpa.py
@@ -2,6 +2,10 @@ from . import link
 
 link_name = "MaxMind website" 
 link_text = "MaxMind website" 
+<<<<<<< HEAD
 link_url = "https://support.maxmind.com/hc/en-us/search?utf8=%E2%9C%93&query=ccpa" 
+=======
+link_url = "https://support.maxmind.com/hc/en-us/search?utf8=%E2%9C%93&query=ccpa"
+>>>>>>> 9152f2e (change link to fix 404 error for maxmind ccpa)
 
 link.xref_links.update({link_name: (link_text, link_url)})
