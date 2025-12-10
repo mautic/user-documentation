bb59f30de1b373dd9059364fbe972c9091f9fa50
Ayu Adiati <retno.ayu98@gmail.com>
2025-12-10 10:08:06 +0100

    fix: broken links, link name and text, adjust wording * feat: rename files by appending word mautic for clarity

---
+++ docs/links/mautic_marketplace_docs.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "Marketplace"
+link_text = "Marketplace"
+link_url = "https://devdocs.mautic.org/en/5.x/marketplace/getting_started.html"
+
+link.xref_links.update({link_name: (link_text, link_url)})
---
+++ docs/links/mautic_plugins_documentation.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "Mautic Plugins" 
+link_text = "Plugins" 
+link_url = "https://devdocs.mautic.org/en/5.x/plugins/getting_started.html" 
+
+link.xref_links.update({link_name: (link_text, link_url)})
