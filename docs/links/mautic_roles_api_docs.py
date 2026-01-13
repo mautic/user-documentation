5e0407a51101e72635a0ebcb1ad19d12c462ecf7
Ayu Adiati <retno.ayu98@gmail.com>
2026-01-13 11:31:24 +0100

    change Roles link from archived repo to dev docs

--- docs/links/mautic_roles_api_docs.py
+++ docs/links/mautic_roles_api_docs.py
@@ -2,6 +2,6 @@ from . import link
 
 link_name = "Roles using the API"
 link_text = "Roles using the API"
-link_url = "https://github.com/mautic/developer-documentation/blob/main/source/includes/_api_endpoint_roles.md"
+link_url = "https://devdocs.mautic.org/en/5.x/rest_api/roles.html"
 
 link.xref_links.update({link_name: (link_text, link_url)})
