29a66acf9b7500e225b0534de904b40d97670f9a
Ayu Adiati <retno.ayu98@gmail.com>
2026-01-26 10:08:17 +0100

    add external link

---
+++ docs/links/php_date_time_formats.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "supported PHP date and time formats" 
+link_text = "supported PHP date and time formats" 
+link_url = "https://www.php.net/manual/en/datetime.formats.php" 
+
+link.xref_links.update({link_name: (link_text, link_url)})
