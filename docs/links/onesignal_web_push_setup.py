b2ba849a630cb96b59df340e59bfd193d57d5367
Ayu Adiati <retno.ayu98@gmail.com>
2026-03-03 04:48:34 +0100

    rename file and fix broken link

---
+++ docs/links/onesignal_web_push_setup.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "OneSignal web push"
+link_text = "OneSignal web push"
+link_url = "https://documentation.onesignal.com/docs/en/web-push-setup"
+
+link.xref_links.update({link_name: (link_text, link_url)})
