62244f59408e5baf8dec7e8c0215c1b13d79dac2
Ayu Adiati <retno.ayu98@gmail.com>
2026-05-24 12:29:10 +0200

    rename files and adjust links

---
+++ docs/links/amazon_create_s3_bucket.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "Create a bucket" 
+link_text = "Create a bucket" 
+link_url = "https://docs.aws.amazon.com/AmazonS3/latest/userguide/GetStartedWithS3.html#creating-bucket"
+
+link.xref_links.update({link_name: (link_text, link_url)})
